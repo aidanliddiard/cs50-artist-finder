cd817986a075f451cff34aaf342d628efa5e02d7
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-03-27 14:22:37 -0700

    trying to merge

---
+++ README.md
@@ -0,0 +1,23 @@
+# YOUR PROJECT TITLE
+
+## Video Demo: URL
+
+### Description:
+
+This app allows a user to search for similar artists to the artist they search for
+
+#### Design Choices:
+
+A goal for this project was level AAA of conformance with the Web Content Accessibility Guidelines (WCAG). In preperation and to learn more about accesible web development I watched several videos and sought out the experience of Evelyn Wightman who was a mentor in accessibility and also tested my project.
+
+I used several accessibility tools while completing my project including:
+
+- WAVE Evalutaion Tool
+-
+
+#### File Architecture:
+
+#### Tools:
+
+- GitHub Copilot
+-
---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css';
+// import { useGetSimilarArtists } from './lib/spotify-api-hooks';
+// import { useGetEvents } from './lib/ticketmaster-api-hooks';
+// import { FetchState } from './types';
+import Artists from './views/Artists';
+
+function App() {
+  //   const [events, fetchStateEvents, getEvents] = useGetEvents()
+  //   const [artists, fetchStateArtists, getSimilarArtists] = useGetSimilarArtists()
+
+  return (
+    <div className="App">
+      <h1>Explore New Music</h1>
+      <Artists />
+      {/* {
+        fetchStateEvents === FetchState.DEFAULT && <button onClick={getEvents}>Get Events</button>
+      }
+      {fetchStateEvents === FetchState.LOADING && <p>Loading...</p>}
+      {fetchStateEvents === FetchState.ERROR && <p>There was an error</p>}
+      {fetchStateEvents === FetchState.SUCCESS && (
+        <ul>
+          {events.map((event) => (
+            <li key={event.id}>
+              <h2>{event.name}</h2>
+              <p>{event.dates.start.localDate} at {event.dates.start.localTime}</p>
+              <p>{event.url}</p>
+            </li>
+          ))}
+        </ul>
+      )} */}
+    </div>
+  );
+}
+
+export default App;
